daaa89c02b11ab2617dacec63ad308f3a065d10c
Sofiia Salikhova <sofiia.salikhova@yandex.ru>
2021-12-22 09:39:39 +0300

    8th commit: text for user was added

--- main.py
+++ main.py
@@ -40,31 +40,50 @@ def gun():
 
 handsDetector = mp.solutions.hands.Hands()
 cap = cv2.VideoCapture(0)
+
 while cap.isOpened():
     ret, frame = cap.read()
+
     if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
         break
+
     flipped = np.fliplr(frame)
     flippedRGB = cv2.cvtColor(flipped, cv2.COLOR_BGR2RGB)
     results = handsDetector.process(flippedRGB)
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    cv2.putText(flippedRGB, 'Show one of 5 gestures (stone, table,', (10, 30), font, 1,
+                (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(flippedRGB, 'water, bird, table)', (350, 60), font, 1,
+                (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(flippedRGB, 'Program recognizes it', (10, 90), font, 1,
+                (255, 255, 255), 2, cv2.LINE_AA)
+
     if results.multi_hand_landmarks is not None:
         (x, y), r = cv2.minEnclosingCircle(get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape))
         ws = palm_size(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)
+
         if stone(r, ws):
             font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(flippedRGB, 'stone', (10, 100), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(flippedRGB, 'stone', (10, 200), font, 4, (255, 0, 0), 2, cv2.LINE_AA)
+
         if table():
             font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(flippedRGB, 'table', (10, 100), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(flippedRGB, 'table', (10, 200), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+
         if water():
             font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(flippedRGB, 'water', (10, 100), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(flippedRGB, 'water', (10, 200), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+
         if bird():
             font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(flippedRGB, 'bird', (10, 100), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(flippedRGB, 'bird', (10, 200), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+
         if gun():
             font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(flippedRGB, 'table', (10, 100), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(flippedRGB, 'gun', (10, 200), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+
     res_image = cv2.cvtColor(flippedRGB, cv2.COLOR_RGB2BGR)
     cv2.imshow("Hands", res_image)
 
